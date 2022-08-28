e25ba83bc017f325e7064de7837b2aeb2db0bd53
ahsouza <annibalhsouza@gmail.com>
2022-08-28 00:17:28 -0300

    feat: add dimension

--- conf.lua
+++ conf.lua
@@ -2,8 +2,15 @@ function love.conf(data)
   data.identity = "data/saved"
   data.version = "1.0.0"
   data.console = true
-  data.externalStorage = true
-  data.gammaCorrect = true
+  data.externalstorage = true
+  data.gammacorrect = true
   data.window.title = "Star Dust"
-  data.window.icon = "assets/stardust_icon.jpg"
+  data.window.icon = "assets/icon/stardust_icon.jpg"
+  -- DIMENSION WINDOW
+  data.window.width = 1200
+  data.window.width = 700  
+  data.window.minheight = 500
+  data.window.minwidth = 920
+  data.window.resizable = true
+
 end
