b823c025fbab695b9900173874801a6db9f6eaf6
ahsouza <annibalhsouza@gmail.com>
2022-08-28 00:09:09 -0300

    feat: new conf game

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(data)
+  data.identity = "data/saved"
+  data.version = "1.0.0"
+  data.console = true
+  data.externalStorage = true
+  data.gammaCorrect = true
+  data.window.title = "Star Dust"
+  data.window.icon = "assets/stardust_icon.jpg"
+end
